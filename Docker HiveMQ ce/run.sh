3efd995f14ef8c0c9aae12396fb16b7a9817a667
Verckor <marvin.kamps@mailbox.org>
2020-09-11 00:02:29 +0200

    clean

--- Docker HiveMQ ce/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#build image
-docker build --no-cache -t hive-mq-ce .
-
-#run image
-#docker run --name hivemq-ce -d -p 8883:8883 hive-mq-ce
-docker run --rm --name hive-mq-ce -d -p 8883:8883 hive-mq-ce
