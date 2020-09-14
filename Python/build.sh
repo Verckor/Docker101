ae33754c7920c71472f1614974954ce828aa6432
Verckor <marvin.kamps@mailbox.org>
2020-09-14 13:22:52 +0200

    clean up

--- Python/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#build image
-docker build -t imagename .
-
--- Python/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#run image
-#starts console mode:
-#docker run --rm imagename
-#background mode
-docker run --rm -d imagename
